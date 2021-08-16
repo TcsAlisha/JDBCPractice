de04a2ac719748eee94a83d73abbc19057ec8366
TcsAlisha <tcsalishadias@gmail.com>
2021-08-16 09:02:10 +0000

    delete,update,retrieve,create

--- src/main/java/com/tcs/jdbc/demo/JDBCDemo.java
+++ src/main/java/com/tcs/jdbc/demo/JDBCDemo.java
@@ -5,31 +5,60 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+import java.util.ArrayList;
+import java.util.List;
 public class JDBCDemo {
 	public static void main(String[] args) {
-		String DB_URL = "jdbc:mysql://localhost:3306/practice";
-		String DB_USER = "root";
-		String DB_PASSWORD = "Nuvelabs123$";
-
-		try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-				Statement statement = connection.createStatement();) {
-//			create(statement); // create
-			retrieve(statement);
+		String DB_URL="jdbc:mysql://localhost/practice";
+		String DB_USER="root";
+		String DB_PASSWORD="Nuvelabs123$";
+		
+		try(Connection connection= DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+				Statement statement = connection.createStatement();){
+			//create(statement);//create operation 
+			//update(statement);
+			//retrieve(statement);
+			//delete(statement);
+			List<String> reg= retrieveWithCondittion(statement,"A");
+			System.out.println(reg);
 		} catch (SQLException e) {
 			e.printStackTrace();
+		};
+	}
+
+	private static List<String> retrieveWithCondittion(Statement statement, String str) throws SQLException {
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM REGIONS WHERE REGION_NAME LIKE '"+str+"%'");
+	
+		List<String> ls = new ArrayList<>();
+		while(resultSet.next()) {
+			System.out.print(resultSet.getInt(1)+" ");
+			System.out.println(resultSet.getNString("REGION_NAME"));
+			ls.add(resultSet.getNString("REGION_NAME"));
 		}
+		System.out.println("");
+		return ls;
+	}
+
+	private static void delete(Statement statement) throws SQLException {
+		statement.execute("DELETE FROM REGIONS WHERE REGION_ID=10");
+	}
+
+	private static void update(Statement statement) throws SQLException {
+		statement.execute("UPDATE REGIONS SET REGION_NAME='India' WHERE REGION_ID=5");
+		statement.execute("UPDATE REGIONS SET REGION_NAME='Africa' WHERE REGION_ID=1");
+		statement.execute("UPDATE REGIONS SET REGION_NAME='America' WHERE REGION_ID=2");
 	}
 
 	private static void retrieve(Statement statement) throws SQLException {
-		ResultSet resultSet = statement.executeQuery("SELECT * from regions");
-		while (resultSet.next()) {
-			System.out.println(resultSet.getInt(1));
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM REGIONS");
+		
+		while(resultSet.next()) {
+			System.out.print(resultSet.getInt(1)+" ");
 			System.out.println(resultSet.getNString("REGION_NAME"));
 		}
+		System.out.println("");
 	}
-
 	private static void create(Statement statement) throws SQLException {
-		statement.execute("INSERT INTO REGIONS VALUES(2, 'North America')");
+		statement.execute("INSERT INTO REGIONS VALUES(5,'Canada')");
 	}
 }
