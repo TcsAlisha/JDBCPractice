4cdf990d5c994788f296f096a70f514ae97bc804
TcsAlisha <tcsalishadias@gmail.com>
2021-08-16 09:36:12 +0000

    log4j

--- src/main/java/com/tcs/jdbc/demo/JDBCDemo.java
+++ src/main/java/com/tcs/jdbc/demo/JDBCDemo.java
@@ -7,12 +7,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.slf4j.Logger; //important
+import org.slf4j.LoggerFactory;
+
+
 public class JDBCDemo {
+	private static final Logger logger = LoggerFactory.getLogger(JDBCDemo.class);
 	public static void main(String[] args) {
 		String DB_URL="jdbc:mysql://localhost/practice";
 		String DB_USER="root";
 		String DB_PASSWORD="Nuvelabs123$";
-		
 		try(Connection connection= DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
 				Statement statement = connection.createStatement();){
 			//create(statement);//create operation 
@@ -50,15 +55,17 @@ public class JDBCDemo {
 	}
 
 	private static void retrieve(Statement statement) throws SQLException {
-		ResultSet resultSet = statement.executeQuery("SELECT * FROM REGIONS");
-		
-		while(resultSet.next()) {
-			System.out.print(resultSet.getInt(1)+" ");
-			System.out.println(resultSet.getNString("REGION_NAME"));
+		ResultSet resultSet = statement.executeQuery("SELECT * from regions");
+		List<String> regions = new ArrayList<String>();
+		while (resultSet.next()) {
+			logger.debug(resultSet.getInt(1)+"");
+			logger.debug(resultSet.getString("REGION_NAME"));
+			regions.add(resultSet.getNString("REGION_NAME"));
+			
 		}
-		System.out.println("");
 	}
+
 	private static void create(Statement statement) throws SQLException {
-		statement.execute("INSERT INTO REGIONS VALUES(5,'Canada')");
+		statement.execute("INSERT INTO REGIONS VALUES(2, 'North America')");
 	}
 }
